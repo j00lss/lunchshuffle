13deaf220a4345535eea93fe7b4d8d542b06dfea
Johan Olsson Bachmann <johanolsson.net@gmail.com>
2015-02-13 08:20:11 +0100

    Callback page for spotify. Vash instead of Jade templates. New Git remote url for Heroku.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,44 @@
+var APP = window.APP || {};
+
+APP.Lunchshuffle = (function Module (w, $) {
+	var self,
+		pub,
+		_foo;
+
+	self = {
+		getSpotifySongs: function (cb) {
+			var spotifySongList = [
+				{
+					id: '',
+					artist: ''
+				}
+			];
+		}
+	};
+
+	return pub = {
+		init: function MainInit () {
+			
+		}
+	};
+}(window, jQuery));
+
+
+jQuery(APP.Lunchshuffle.init());
+
 $(function() {
-	$('#buttonWrap').on('mouseleave', function () {
+/*	$('#buttonWrap').on('mouseleave', function () {
 		window.location.reload();
 	});
-})
+})*/
+
+/*
+var songs = [
+	'4zzXnAXdU0ilshAdLgyYBT',
+	'6BtHjUHuNYh7w5daOE6iBY',
+	'4nQNNwnIUUmFzVOcVVJCk1',
+	'0RabJGGnKLiiW6EeMAyh3p',
+	'221mzU0izy6JdU1zRlIMAM',
+	'0m8BLDLvxup9iB3XkId4Jz',
+	'4i2TDkP0zm4CxYkmWW6EX4'
+];*/
--- web.js
+++ web.js
@@ -1,31 +1,49 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyScopes = ['user-read-private'],
+	spotifyState = 'some-state-of-my-choice';
+	spotifyClientId = '580224fcb345437ebea001635c15a587',
+	spotifyClientSecret = '7f94649fd6a142a190482081fdc158b6',
+	spotifyRedirectUri = 'http://localhost:5000/cb';
+
+var spotifyApi = new SpotifyWebApi({
+	clientId : spotifyClientId,
+  	clientSecret : spotifyClientSecret,
+  	redirectUri: spotifyRedirectUri
+});
+
+var authorizeURL = spotifyApi.createAuthorizeURL(spotifyScopes, spotifyState);
+
+/*spotifyApi.getPlaylist('joolss', '3mBXQXXuEkx3PE2Zwm7vBd')
+  .then(function(data) {
+    console.log('Some information about this playlist', data);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });*/
+
 var app = express();
 
-var songs = [
-	'4zzXnAXdU0ilshAdLgyYBT',
-	'6BtHjUHuNYh7w5daOE6iBY',
-	'4nQNNwnIUUmFzVOcVVJCk1',
-	'0RabJGGnKLiiW6EeMAyh3p',
-	'221mzU0izy6JdU1zRlIMAM',
-	'0m8BLDLvxup9iB3XkId4Jz',
-	'4i2TDkP0zm4CxYkmWW6EX4'
-];
-
 app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
+app.set('view engine', 'vash')
 
 app.use(logfmt.requestLogger());
 app.use(express.static(__dirname))
 
-app.get('/', function(req, res) {
+app
+	.get('/', function(req, res) {
 	//res.send('Hello World!');
 	res.render('index', { 
-		pageTitle: 'Shuffle it',
-		hello: 'Lunch shuffle! (demo) v0.1',
-		songId: songs[Math.floor( Math.random() * songs.length)]
+		pageTitle: 'Lez shuffle it',
+		hello: 'Lunch shuffle! (demo) v0.2',
+		//songId: songs[Math.floor( Math.random() * songs.length)]
 	});
-});
+	}).get('/cb', function(req, res) {
+		res.render('cb', { 
+			pageTitle: 'Callback page',
+		});
+	});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
